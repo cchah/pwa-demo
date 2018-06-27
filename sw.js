c15b426a1bf13f2f8afc62125e8f2ba034819284
cchah <748648443@qq.com>
2018-06-27 17:19:49 +0800

    pwademo

--- sw.js
+++ sw.js
@@ -15,30 +15,3 @@ self.addEventListener('install', e => {
   )
 })
 
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      if (response != null) {
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    Promise.all(
-      caches.keys().then(cacheNames => {
-        return cacheNames.map(name => {
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-        })
-      })
-    ).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
