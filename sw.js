e88d388874d8c5c20b6b4c20ed9fa37250ad2581
cchah <748648443@qq.com>
2018-06-27 19:13:33 +0800

    pwademo

--- sw.js
+++ sw.js
@@ -14,6 +14,48 @@ self.addEventListener('install', e => {
     .then(cache => cache.addAll(cacheList))
     .then(() => self.skipWaiting())
   )
-})
+});
+
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  if (requestUrl.origin === location.origin) {
+      if (requestUrl.pathname === '/') {
+      event.respondWith(
+        caches.open(cacheStorageKey).then(function(cache) {
+          return fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          }).catch(function() {
+            return cache.match(event.request);
+          });
+        })
+      );
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if ( cacheStorageKey.indexOf(cacheName) === -1 ) {
+            // When it doesn't match any condition, delete it.
+            console.info('SW: deleting ' + cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
 
 
